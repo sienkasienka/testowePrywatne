1d4c5795f5d69e5136888c730a71fe6e42cdd290
sienkasienka <32477659+sienkasienka@users.noreply.github.com>
2019-12-08 21:26:30 +0100

    DodaneZGithuba1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("1 commit z kompa przed inicjalizacja Repo");
-        System.out.println("1 commit z kompa przed inicjalizacja Repo");
+        Dodane z githuba z celu zasymulowania zmian przez contrubitora
     }
 
 }

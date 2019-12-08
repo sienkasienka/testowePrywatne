218266fadf4271d81010d8dbd5c7614b9b45795e
sienkasienka <32477659+sienkasienka@users.noreply.github.com>
2019-12-08 21:33:08 +0100

    DodanieClassyZGitHjuba

---
+++ src/main/java/Main2.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main2 {
+    public static void main(String[] args) {
+    Classa stworzona z githuba w celu zasymulowania dodania nowej klasy przez cobtributoa
+    }
+
+}

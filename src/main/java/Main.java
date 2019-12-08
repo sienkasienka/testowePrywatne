343fc7262c1b374ccbaf5f7a471968e4383a0d8a
MSI <sienka_sienka@op.pl>
2019-12-08 21:36:37 +0100

    added 3 linies before commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("1 commit z kompa przed inicjalizacja Repo");
+        System.out.println("3 commit z kompa w komencie gdy dodalem jedna linie kodu z Githuba");
+        System.out.println("DodanieLini z kompa gdy na githubie sunalem pare linii);
+        System.out.println("1 commit z kompa przed inicjalizacja Repo");
         Dodane z githuba z celu zasymulowania zmian przez contrubitora
     }
 

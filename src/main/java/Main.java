46e8c088429157f22509b9e411ee684223e86d43
MSI <sienka_sienka@op.pl>
2019-12-08 21:27:58 +0100

    third commit form comp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("1 commit z kompa przed inicjalizacja Repo");
+        System.out.println("3 commit z kompa w komencie gdy dodalem jedna linie kodu z Githuba");
         System.out.println("1 commit z kompa przed inicjalizacja Repo");
     }
 
